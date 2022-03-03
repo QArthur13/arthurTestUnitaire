d9c4d272e01ee9d23479a42054cff3ae0c646482
Quaranta Arthur <arthur.quaranta@ynov.com>
2022-03-03 17:32:15 +0100

    First Commit

--- Back/tests/ApiTest.php
+++ Back/tests/ApiTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Service\RickAndMortyGestion;
 
 class ApiTest extends WebTestCase
 {
@@ -16,5 +17,18 @@ class ApiTest extends WebTestCase
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['message' => "Hello world"], $responseData);
+    }
+
+    public function testApiProducts(): void
+    {
+        $client = static::createClient();
+        $client->jsonRequest('GET', '/api/products');
+
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertTrue(true);
     }
 }
